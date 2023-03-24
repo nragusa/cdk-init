33462c1922379a94110b37335be3f6d77e3c847d
nragusa <nragusa@gmail.com>
2023-03-24 18:49:16 +0000

    updates

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,5 +3,5 @@ import subprocess
 app_name = '{{ cookiecutter.project_name}}'
 
 subprocess.run(
-    ['/home/ec2-user/.nvm/versions/node/v16.4.1/bin/cdk', 'init', '--language=python']
+    ['/home/ec2-user/.nvm/versions/node/v18.15.0/bin/cdk', 'init', '--language=python']
 )
