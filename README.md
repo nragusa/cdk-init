b404bac79093c18ff8d08fbd585ab65d10ed2b4e
Nick Ragusa <nragusa@gmail.com>
2025-02-12 17:31:12 -0500

    updates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Overview
 
-A very basic [cookiecutter](https://cookiecutter.readthedocs.io/en/stable/) template to initialize an AWS CDK application using `pipenv` instead of `virtualenv`.
+A very basic [cookiecutter](https://cookiecutter.readthedocs.io/en/stable/) template to initialize an AWS CDK application using ~~`pipenv`~~ `poetry` instead of `virtualenv`.
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import subprocess
+import sys
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -20,9 +21,13 @@ if __name__ == '__main__':
     remove_file('requirements.txt')
     remove_file('requirements-dev.txt')
     remove_directory('.venv/')
-    subprocess.run(
-        ['poetry', 'add', 'aws-cdk-lib'],
-        capture_output=True,
-        text=True,
-        check=True
-    )
+    try:
+        subprocess.run(
+            ['poetry', 'add', 'aws-cdk-lib'],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error running 'poetry add aws-cdk-lib': {e}")
+        sys.exit(1)
