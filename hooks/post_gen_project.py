f6f22f80840c412867ef634d0ef55bd9a08c5c12
Nick Ragusa <nragusa@gmail.com>
2025-03-10 13:24:34 -0400

    updated environment variable to specify the default AWS region

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,5 @@
+"""Post-generation project hook."""
+
 import os
 import shutil
 import subprocess
@@ -7,41 +9,26 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
+    """Remove a file from the project directory."""
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
 def remove_directory(directory):
-    shutil.rmtree(os.path.join(PROJECT_DIRECTORY,
-                  directory), ignore_errors=True)
+    """Remove a directory from the project directory."""
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, directory), ignore_errors=True)
 
 
 if __name__ == '__main__':
-
     remove_file('requirements.txt')
     remove_file('requirements-dev.txt')
     remove_directory('.venv/')
     try:
-        subprocess.run(
-            ['zsh', '-i', '-c', 'venv'],
-            capture_output=True,
-            text=True,
-            check=True
-        )
-        subprocess.run(
-            ['uv', 'add', 'aws-cdk-lib'],
-            capture_output=True,
-            text=True,
-            check=True
-        )
+        subprocess.run(['zsh', '-i', '-c', 'venv'], capture_output=True, text=True, check=True)
+        subprocess.run(['uv', 'add', 'aws-cdk-lib'], capture_output=True, text=True, check=True)
         with open('.envrc', 'a') as f:
             f.write('export AWS_PROFILE=sso\n')
-            f.write('export AWS_REGION=us-east-2\n')
-        subprocess.run(
-            ['direnv', 'allow'],
-            capture_output=True,
-            text=True,
-            check=True
-        )
+            f.write('export AWS_DEFAULT_REGION=us-east-2\n')
+        subprocess.run(['direnv', 'allow'], capture_output=True, text=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running 'venv': {e}")
         sys.exit(1)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,15 +1,12 @@
+"""Pre-generation project hook."""
+
 import subprocess
 import sys
 
 app_name = '{{cookiecutter.project_name}}'
 
 try:
-    subprocess.run(
-        ['cdk', 'init', '--language=python'],
-        capture_output=True,
-        text=True,
-        check=True
-    )
+    subprocess.run(['cdk', 'init', '--language=python'], capture_output=True, text=True, check=True)
 except subprocess.CalledProcessError as e:
     print(f"Error running 'cdk init': {e}")
     sys.exit(1)
